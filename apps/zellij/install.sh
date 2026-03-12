576ed21500adb5712f6c207ecdf65e7b4fcc3269
Otto Hermann <ojhermann@gmail.com>
2026-03-12 18:18:12 -0400

    create zj layouts directory

--- apps/zellij/install.sh
+++ apps/zellij/install.sh
@@ -7,6 +7,7 @@ print -P "%F{green}%B$APP%b: starting to install $APP%f"
 if ! command -v zellij &>/dev/null; then
 	brew install zellij
 	ln -sf ~/Documents/configuration_apple_os/apps/zellij/config.kdl ~/.config/zellij/config.kdl
+	ln -sf ~/Documents/configuration_apple_os/apps/zellij/layouts ~/.config/zellij/layouts
 fi
 
 print -P "%F{green}%B$APP%b: $APP has been installed%f"
