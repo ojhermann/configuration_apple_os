53c97d4367915e7d43c34a16bc2bc8839bb59a84
Otto J. Hermann <ojhermann@gmail.com>
2026-03-13 17:37:36 -0400

    mise autocomplete (#18)

--- apps/mise/install.sh
+++ apps/mise/install.sh
@@ -7,6 +7,11 @@ print -P "%F{green}%B$APP%b: starting to install $APP%f"
 if ! command -v mise &>/dev/null; then
 	curl https://mise.run | sh
 	echo "eval \"\$(~/.local/bin/mise activate zsh)\"" >>~/.zshrc
+	source ~/.zshrc
+	# https://mise.jdx.dev/installing-mise.html#autocompletion
+	mise use -g usage
+	mkdir -p ~/.zfunc && mise completion zsh >~/.zfunc/_mise
+	grep -qxF 'fpath=(~/.zfunc $fpath)' ~/.zshrc || echo 'fpath=(~/.zfunc $fpath)' >>~/.zshrc
 fi
 
 print -P "%F{green}%B$APP%b: $APP has been installed%f"
