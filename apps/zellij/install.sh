3556f43e69a173046f39dfe1fb1bf38a249bd6f8
Otto Hermann <ojhermann@gmail.com>
2026-03-12 20:20:58 -0400

    correct zellij install

--- apps/zellij/install.sh
+++ apps/zellij/install.sh
@@ -6,8 +6,13 @@ print -P "%F{green}%B$APP%b: starting to install $APP%f"
 
 if ! command -v zellij &>/dev/null; then
 	brew install zellij
+	if [[ ! -d ~/.config/zellij/layouts ]]; then
+		mkdir -p ~/.config/zellij/layouts
+	fi
 	ln -sf ~/Documents/configuration_apple_os/apps/zellij/config.kdl ~/.config/zellij/config.kdl
 	ln -sf ~/Documents/configuration_apple_os/apps/zellij/layouts ~/.config/zellij/layouts
 fi
 
 print -P "%F{green}%B$APP%b: $APP has been installed%f"
+
+# todo make sure config overwrites the default and does not get overwritten
