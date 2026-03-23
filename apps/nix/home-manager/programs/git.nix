3d6ae4cf9ca407062cfc724a698db334a9a6e199
Otto Hermann <ojhermann@gmail.com>
2026-03-23 18:50:03 -0400

    corrections and git

--- apps/nix/home-manager/programs/git.nix
+++ apps/nix/home-manager/programs/git.nix
@@ -2,12 +2,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "Otto Hermann";
-    userEmail = "ojhermann@gmail.com";
-    aliases = {
-      up = "!git remote update -p; git merge --ff-only @{u}";
-    };
-    extraConfig = {
+    settings = {
+      user.name = "Otto Hermann";
+      user.email = "ojhermann@gmail.com";
+      alias = {
+        up = "!git remote update -p; git merge --ff-only @{u}";
+      };
       init.defaultBranch = "main";
       pull.ff = "only";
       push.autoSetupRemote = true;
