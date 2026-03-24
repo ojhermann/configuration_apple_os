5e121bff4cdc6ae89dc25b76a47dc52782a413ea
Otto J. Hermann <ojhermann@gmail.com>
2026-03-24 00:24:18 -0400

    aliases were removed (#23)

--- aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-# shellcheck shell=bash
-
-alias date="date +'%Y-%m-%d %H:%M:%S'"
-alias grep="grep -i --color=auto"
-alias gs="git status -sb"
-alias ls="ls --color=auto"
-alias tree="tree -aC"
-alias zj="zellij"
