21f9ac52c6cfa8d07a6f71ba5ab80b85e72a0d34
Otto Hermann <ojhermann@gmail.com>
2026-03-23 22:05:03 -0400

    alacritty -> nix

---
+++ apps/nix/home-manager/programs/alacritty.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.alacritty ];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      mouse.hide_when_typing = true;
+
+      window.startup_mode = "Fullscreen";
+
+      keyboard.bindings = [
+        {
+          key = "Return";
+          mods = "Shift";
+          chars = "\u001B\r";
+        }
+      ];
+    };
+  };
+}
